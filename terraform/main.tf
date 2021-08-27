541ffb4e54410f46085dffd6dbff16e15bd426b7
Yvon QUEROMES <yvon.queromes@metanext.com>
2021-08-27 11:05:05 +0200

    27981104

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,10 +13,7 @@ terraform {
 
 provider "azurerm" {
     version         =   "~> 2.0"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    subscription_id =   var.subscription_id
-    tenant_id       =   var.tenant_id
+    
     
     features {}
 }
@@ -26,9 +23,7 @@ provider "azurerm" {
 
 provider "azuread" {
     version         =   ">= 0.11"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    tenant_id       =   var.tenant_id
+    
     alias           =   "ad"
 }
 
